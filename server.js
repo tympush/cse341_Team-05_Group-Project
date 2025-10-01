7bb25311ece3adcdb092b2ddda80344d3e4d781a
tympush <tymur.pushnoy@gmail.com>
2025-10-01 13:30:21 +0200

    added mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-//const mongodb = require("./db/connect");
+const mongodb = require("./db/connect");
 const passport = require("passport");
 const session = require("express-session");
 const GitHubStrategy = require("passport-github2").Strategy;
@@ -65,7 +65,7 @@ process.on("uncaughtException", (err, origin) => {
 
 
 
-/*mongodb.initDb((err) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
@@ -73,4 +73,4 @@ process.on("uncaughtException", (err, origin) => {
       console.log(`Connected to DB and listening on ${port}`);
     });
   }
-});*/
+});
