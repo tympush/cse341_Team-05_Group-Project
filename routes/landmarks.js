859fd9169a87795248a367db453bcb960313aa34
tympush <tymur.pushnoy@gmail.com>
2025-10-02 13:52:40 +0200

    merged get and post methods for landmarks

--- routes/landmarks.js
+++ routes/landmarks.js
@@ -6,6 +6,11 @@ const { isAuthenticated } = require("../middleware/authenticate");
 
 const landmarksController = require("../controllers/landmarks");
 
+router.get('/', landmarksController.getAll);
+
+router.get('/:id', landmarksController.getOne);
+
+router.post('/', isAuthenticated, validation.validateLandmark, landmarksController.createLandmark);
 
 router.put("/:id", isAuthenticated, validation.validateLandmark, landmarksController.updateLandmark);
 
